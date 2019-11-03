45f07779843b2a3c16dd802a395d21e44d093791
zhanwang <545578254@qq.com>
2019-11-03 21:50:34 +0800

    测试

--- js/main.js
+++ js/main.js
@@ -1,90 +1,21 @@
-// /**
-//  * @role 小地图
-//  * @time 2019.06.20
-//  */
+$('.name').typeIt({
+    whatToType: ["亲爱的晶姐："],
+    typeSpeed: 100
+  },
+  function() {
+    $('.title').addClass("hide")
+    $('.title').typeIt({
+        whatToType: ["祝你生日快乐，祝你百岁无忧", "也祝世界和平，祝你愿望都实现。"],
+        typeSpeed: 100
+      },
+      function() {
+        $('.title').addClass("hide")
+        $('.main').typeIt({
+            whatToType: ["就算见不到你，", "我也想亲口告诉你：我爱你", "爱着你，就像老鼠爱大米~"],
+            typeSpeed: 100
+          },
+          function() {
 
-var mousePos
-$(".littleimg").css("width", $("#imageid").width())
-var screenImage = $("#imageid");
-var trueImg = $(".longimg");
-var theImage = new Image();
-theImage.src = trueImg.attr("src");
-var trueWidth = $(".longimg").width()
-var lightWidth = $(".contrl").width() * ($(window).width() / trueWidth)
-$(".light").width(lightWidth)
-var imageWidth = screenImage.width();
-var imageHeight = screenImage.height();
-var flag = false　　
-console.log($(".longimg").width());
-
-
-function mousePosition(ev) {   
-    if (ev.pageX || ev.pageY) {   
-        return {
-            x: ev.pageX,
-            y: ev.pageY
-        };   
-    }   
-    return {    
-        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-        y: ev.clientY + document.body.scrollTop  - document.body.clientTop    
-    }; 
-}
-
-
-function mouseMove(ev) {  
-    ev = ev || window.event;  
-    mousePos = mousePosition(ev);  
-
-}
-document.onmousemove = mouseMove;
-
-function Show(el) {   
-    var x = parseInt(mousePos.x) - el.offsetLeft - (lightWidth / 2)
-    var y = parseInt(mousePos.y) - el.offsetTop;   
-    var trueLeft = trueWidth - $(".scoll").width()
-    var z =  x / imageWidth * trueWidth
-    var m = $("#imageid").width() - $(".light").width()
-    var n = $(".light").width()
-
-    if (x > m) {
-        x = m
-    }
-    if (x < 0) {
-        x = 0
-    }
-
-    $(".light").css("left", x)
-    $(".littleimg").css("left", -x)
-    $(".scoll").animate({
-        "scrollLeft": z
-    })
-
-}
-$(".scoll").on("mouseover mouseout", function(event) {
-    if (event.type == "mouseover") {
-        flag = true
-
-        //鼠标悬浮
-    } else if (event.type == "mouseout") {
-        //鼠标离开
-
-        flag = false
-    }
-})
-
-
-
-$(".scoll").scroll(function(params) {
-    let x = $(this).scrollLeft()
-    let y = trueWidth - $(".scoll").width()
-    let z = x / y
-    let m = $("#imageid").width() - $(".light").width()
-    let n = m * z
-    if (flag) {
-
-        $(".light").css("left", n)
-        $(".littleimg").css("left", -n)
-
-    }
-})
+          })
+      });
+  })
